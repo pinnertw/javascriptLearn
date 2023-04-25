9af685e6e556127bc8c2980134f16bd20cd16bed
pinnertw <wayne567tw@yahoo.com.tw>
2023-04-25 14:28:41 +0200

    Promise. Done syntax learn, begin react

---
+++ done/Promise.js
@@ -0,0 +1,26 @@
+// Iterable of Promises.
+Promise.all([
+  new Promise(resolve => setTimeout(() => resolve(1), 3000)), // 1
+  new Promise(resolve => setTimeout(() => resolve(2), 2000)), // 2
+  new Promise(resolve => setTimeout(() => resolve(3), 1000))  // 3
+]).then(console.log); // 1,2,3 
+
+// Promise.allSettled
+let urls = [
+  'https://api.github.com/users/iliakan',
+  'https://api.github.com/users/remy',
+  'https://no-such-url'
+];
+
+Promise.allSettled(urls.map(url => fetch(url)))
+  .then(results => { // (*)
+    results.forEach((result, num) => {
+      if (result.status == "fulfilled") {
+        alert(`${urls[num]}: ${result.value.status}`);
+      }
+      if (result.status == "rejected") {
+        alert(`${urls[num]}: ${result.reason}`);
+      }
+    });
+  });
+// race,  any
